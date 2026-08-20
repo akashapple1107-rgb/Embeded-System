149ea12c5f0411f260035dae1a008e15fadf9644
akashapple1107-rgb <akashapple1107@gmail.com>
2026-08-20 23:21:57 +0530

    Task 2.1P commit

---
+++ 2.1P/sketch_aug20a/sketch_aug20a.ino
@@ -0,0 +1,123 @@
+#include <Wire.h>
+#include <DHT.h>
+#include <BH1750.h>
+#include <WiFiNINA.h>
+#include <ThingSpeak.h>
+
+// setting pins and defining type for dht sensor
+#define DHTPIN 3
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+BH1750 lightsensor;
+
+// id and pass for my wifi
+char ssid[] = "Redmi Note 13 5G";
+char password[] = "akash@123";
+
+// channel id and api key from thing speak
+unsigned long channelID = 3463941;
+const char* writeAPIKey = "D04DP24UKPIULRVC";
+
+WiFiClient client;
+
+// this function helps in connecting to wifi
+void connectWiFi() {
+
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+
+    WiFi.begin(ssid, password);
+
+    delay(5000);
+
+    Serial.print(".");
+  }
+// prints connected if it works
+  Serial.println();
+  Serial.println("WiFi connected!");
+}
+
+// funtion to read temperature
+float readTemperature() {
+
+  float temperature = dht.readTemperature();
+  //returns the temperature
+  return temperature;
+}
+
+// function to read light level
+float readLight() {
+
+  float light = lightsensor.readLightLevel();
+  //returns the light level
+  return light;
+}
+
+// this function sends data to thing speak 
+void sendData(float temperature, float light) {
+
+  ThingSpeak.setField(1, temperature);
+  ThingSpeak.setField(2, light);
+
+  int response = ThingSpeak.writeFields(channelID, writeAPIKey);
+
+  if (response == 200) {
+    Serial.println("Data successfully sent to ThingSpeak!");
+  } 
+  else { //this else helps to tell if any problem accured while connecting
+    Serial.print("ThingSpeak error: ");
+    Serial.println(response);
+  }
+}
+
+// Setup code starting everything
+void setup() {
+//defining serial port
+  Serial.begin(9600);
+
+  dht.begin();
+
+  Wire.begin();
+  lightsensor.begin();
+
+  ThingSpeak.begin(client);
+
+  connectWiFi();
+
+  Serial.println("System ready!");
+}
+
+// here every funtion is called 
+void loop() {
+
+  float temperature = readTemperature();
+  float light = readLight();
+
+// this prints temp and light level in serial monitor
+  Serial.println("");
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Light: ");
+  Serial.print(light);
+  Serial.println(" lux");
+
+// exeption handelling
+  if (isnan(temperature)) {
+
+    Serial.println("Temperature reading failed!");
+
+  } 
+  else {
+
+    sendData(temperature, light);
+  }
+
+  Serial.println("Wait 30 seconds");
+
+  delay(30000);
+}
